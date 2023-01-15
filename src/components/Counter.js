82eb5bbaee2d35a0aad187911c483f37c0633090
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-01-15 12:42:04 +0300

    completed assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,9 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
+  //to increment the counter a first time
    setCount(count => (count+1));
+  //to increment the counter a second time
    setCount(count => (count+1))
   }
 
